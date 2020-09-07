c8c29efcf294a43e72b3617b214e588e4ad48868
EC2 Default User <ec2-user@ip-172-31-67-83.ec2.internal>
2020-09-07 11:22:37 +0000

    added logo, login btn, navbar, social links and carousel with overlay

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'gamer-reviews'
+app.config["MONGO_URI"] = 'mongodb+srv://joe:Drumgoon6894@gamerreview.85ibj.mongodb.net/gamer-reviews?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello world ... again'
+@app.route('/get_reviews')
+def get_reviews():
+    return render_template("reviews.html", reviews=mongo.db.reviews.find())
     
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), 
-    port=int(os.environ.get('PORT')),
-    debug=True)
-    
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
