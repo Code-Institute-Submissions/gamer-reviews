419a254406d29e0e2a131d7bf078937103b38522
EC2 Default User <ec2-user@ip-172-31-67-83.ec2.internal>
2020-09-19 08:15:09 +0000

    added contact.html, single-review.html and write-a-review.html

--- app.py
+++ app.py
@@ -27,6 +27,18 @@ def single_article():
 def reviews():
     return render_template("reviews.html", reviews=mongo.db.reviews.find())
 
+@app.route('/single_review')
+def single_review():
+    return render_template("single-review.html", reviews=mongo.db.reviews.find())
+    
+@app.route('/write_review')
+def write_review():
+    return render_template("write-review.html", review=mongo.db.reviews.find())
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', reviews=mongo.db.reviews.find())
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
