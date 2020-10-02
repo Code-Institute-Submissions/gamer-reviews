a4fc4a63ff96c289883da6669c81bd2384df7d93
EC2 Default User <ec2-user@ip-172-31-67-83.ec2.internal>
2020-10-02 09:21:39 +0000

    added login/register modal

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)
 
 app.config["MONGO_DBNAME"] = 'gamer-reviews'
 app.config["MONGO_URI"] = 'mongodb+srv://joe:Drumgoon6894@gamerreview.85ibj.mongodb.net/gamer-reviews?retryWrites=true&w=majority'
---
+++ static/js/modal.js
@@ -0,0 +1,10 @@
+function modalSwitchLogin() {
+    $(".modal-login").show();
+    $(".modal-register").hide();
+}
+
+function modalSwitchRegister() {
+    $(".modal-login").hide();
+    $(".modal-register").show();
+    
+}
