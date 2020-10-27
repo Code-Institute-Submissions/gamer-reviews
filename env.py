647f9b85f9e1c349661ce9428375613f8b2d21f5
EC2 Default User <ec2-user@ip-172-31-67-83.ec2.internal>
2020-10-27 11:47:33 +0000

    added gitignore file

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "241064")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://joe:Drumgoon6894@gamerreview.85ibj.mongodb.net/gamer-reviews?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "gamer-review")
